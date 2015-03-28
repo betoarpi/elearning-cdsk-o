c4d6fb6a08832a93fab78f7f3d825f28c4490e5b
betoarpi <betoarpi@gmail.com>
2015-03-27 23:28:50 -0600

    Menu Empresas home listo

--- wp-content/themes/cdsko-theme/modules/empresas-sistema.php
+++ wp-content/themes/cdsko-theme/modules/empresas-sistema.php
@@ -5,85 +5,85 @@
         <a href="#" class="EmpresasSistema-toggle"><span class="glyphicon glyphicon-menu-down"></span></a>
         <ul>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/arca-continental/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-arca.jpg" alt="">
                     <span>Embotelladora Acra Continental</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/bepensa/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-bepensa_logo.jpg" alt="">
                     <span>Bepensa</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/coca-cola-femsa/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-Femsa_Logo.jpg" alt="">
                     <span>Coca-Cola FEMSA</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/coca-cola-de-mexico/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-CocaColaMexico_logo.jpg" alt="">
                     <span>Coca-Cola de México</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/embotelladora-de-colima/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-EmbotelladoraColima_logo.jpg" alt="">
                     <span>Embotelladora de Colima</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/corporacion-del-fuerte/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-CorporaciondelFuerte_logo.jpg" alt="">
                     <span>Corporación Del Fuerte</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/jugos-del-valle/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-delvalle.jpg" alt="">
                     <span>Jugos del Valle</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/embotelladora-de-nayar/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-nayar.jpg" alt="">
                     <span>Embotelladora de Nayar</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/corporacion-rica/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-rica.jpg" alt="">
                     <span>Corporación Rica</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/industria-envasadora-de-queretaro/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-envasadora-queretaro.jpg" alt="">
                     <span>Industria Envasadora de Querétaro</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/bebidas-refrescantes-nogales/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-brn.jpg" alt="">
                     <span>Bebidas Refrescantes Nogales</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/asociacion-de-embotelladoras-mexicanas/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-asociacion-embotelladoras.jpg" alt="">
                     <span>Asociación de Embotelladoras Mexicanas</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/santa-clara/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-santa-clara.jpg" alt="">
                     <span>Santa Clara</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="<?php bloginfo('siteurl'); ?>/embotelladoras/pet-star/">
                     <img src="<?php bloginfo('template_directory'); ?>/theme-assets/img/empresas-sistema/logo-petstar-low.jpg" alt="">
                     <span>Pet Star</span>
                 </a>
