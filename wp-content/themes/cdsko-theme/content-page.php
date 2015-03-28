6b3379e3b4cca4fdb128a5a057ace2f95cba5577
betoarpi <betoarpi@gmail.com>
2015-03-27 20:42:40 -0600

    Plantilla para cursos con condicional Logeado/No Logueado

--- wp-content/themes/cdsko-theme/content-page.php
+++ wp-content/themes/cdsko-theme/content-page.php
@@ -12,6 +12,20 @@
         <?php if ( is_tree(85) || is_tree(69) ) ://Load Content Slides
             get_template_part( 'modules/content', 'slides' );
         else : endif;?>
-        <?php the_content(); ?>
+		
+		<?php if ( is_page_template('iniciativa-sistema.php') ) :
+			if ( is_user_logged_in() ) {
+				if ( is_page_template('iniciativa-sistema.php') ) ://Load Course Shortcode
+			   		if( get_field('shortcode_del_curso') ):
+			   			$shortcourse = get_field('shortcode_del_curso');
+			           	echo do_shortcode($shortcourse);
+			        endif;
+			    endif;
+			} else {
+				the_content();
+			}
+		else : 
+			the_content(); 
+		endif; ?>
 	</article>
 </div>
